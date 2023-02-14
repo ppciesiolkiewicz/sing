66b1f2eeab3c42b16b6efc4a58ece58e52250fb8
Piotr Ciesiolkiewicz <piotr.c@welysn.com>
2023-02-14 17:42:24 +1100

    TMP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,115 @@
+"use client";
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import { useRef, useState, useEffect, useCallback } from 'react';
+import { PitchDetector as PD } from 'pitchy';
+
+type PitchDetectorProps = {
+    clarityThreshold: number;
+    analyserMinDecibels: number;
+    analyserMaxDecibels: number;
+    analyserSmoothingTimeConstant: number;
+    cb: (pitch: number, clarity: number) => void;
+};
+
+function PitchDetector({
+    clarityThreshold,
+    analyserMinDecibels,
+    analyserMaxDecibels,
+    analyserSmoothingTimeConstant,
+    cb,
+}: PitchDetectorProps): null {
+
+    const updatePitch = useCallback(
+        (analyserNode, detector, input, sampleRate) => {
+            analyserNode.getFloatTimeDomainData(input);
+            const [pitch, clarity] = detector.findPitch(input, sampleRate);
+
+            window.requestAnimationFrame(() => updatePitch(analyserNode, detector, input, sampleRate));
+              cb(pitch, clarity);
+        },
+        [clarityThreshold]
+    );
+
+    useEffect(() => {
+        if (!navigator.getUserMedia) {
+            alert('Your browser cannot record audio. Please switch to Chrome or Firefox.');
+            return;
+        }
+
+        navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+            const audioContext = new window.AudioContext();
+            const analyserNode = audioContext.createAnalyser();
+            analyserNode.minDecibels = analyserMinDecibels;
+            analyserNode.maxDecibels = analyserMaxDecibels;
+            analyserNode.smoothingTimeConstant = analyserSmoothingTimeConstant;
+
+            const sourceNode = audioContext.createMediaStreamSource(stream);
+            sourceNode.connect(analyserNode);
+            const detector = PD.forFloat32Array(analyserNode.fftSize);
+            const input = new Float32Array(detector.inputLength);
+            updatePitch(analyserNode, detector, input, audioContext.sampleRate);
+        });
+    }, [analyserMaxDecibels, analyserMinDecibels, analyserSmoothingTimeConstant, updatePitch]);
+
+    return null;
+}
+
+PitchDetector.defaultProps = {
+    clarityThreshold: 0.98,
+    analyserMinDecibels: -35,
+    analyserMaxDecibels: -10,
+    analyserSmoothingTimeConstant: 0.85,
+};
+
 
 const inter = Inter({ subsets: ['latin'] })
 
+
+const UPDATE_RATE_MS = 200;
+
 export default function Home() {
+  const canvas = useRef<any>(null);
+  const pitch = useRef<[number, number][]>([]);
+  const processedPitch = useState<number[]>([]);
+  const processedPitchLastIndex = useState<number>();
+  const [updateIdx, setUpdateIdx] = useState<number>(0);
+  const setPitchCb = (p: number, c: number) => {
+    // console.log(p)
+    pitch.current = [...pitch.current, [p, c]];
+  };
+
+  useEffect(() => {
+    const t = setTimeout(() => {
+      setUpdateIdx(updateIdx + 1)
+    }, UPDATE_RATE_MS);
+
+    if (canvas.current && pitch.current.length > 0) {
+      const cvs = canvas.current;
+      const width = cvs.width;
+      const height = cvs.height;
+      const ctx = cvs.getContext("2d");
+      ctx.fillStyle = "#FF0000";
+
+      const x = updateIdx;
+      const y = (height - pitch.current[pitch.current.length - 1][0] )/ 10 + height/2;
+      console.log(x, y, pitch.current[pitch.current.length - 1][0])
+      ctx.fillRect(x, y, 2, 2);
+    }
+
+    return () => clearTimeout(t);
+  }, [updateIdx, setUpdateIdx])
+
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <PitchDetector cb={setPitchCb} />
+      {/* {JSON.stringify(pitch, null, '  ')} */}
+      {JSON.stringify(updateIdx, null, '  ')}
+      <canvas style={{ width: '100%', height: '500px', background: 'yellow' }}id="canvas" ref={canvas}>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </canvas>
     </main>
   )
 }
