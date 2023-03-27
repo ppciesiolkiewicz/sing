53b3e20f6c55beb5bc59fac4a8cc5835325ae037
Piotr Ciesiolkiewicz <piotr.c@welysn.com>
2023-03-28 00:50:09 +1100

    New instruments

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -62,6 +62,17 @@ export const DIFFICULTY_LEVEL_OPTIONS = [
 
 export const INSTRUMENT_PIANO1 = 'INSTRUMENT_PIANO1'
 export const INSTRUMENT_PIANO2 = 'INSTRUMENT_PIANO2'
+export const INSTRUMENT_ACCORDION = 'INSTRUMENT_ACCORDION'
+export const INSTRUMENT_BASSOON = 'INSTRUMENT_BASSOON'
+export const INSTRUMENT_CELLO = 'INSTRUMENT_CELLO'
+export const INSTRUMENT_ELEC_PIANO = 'INSTRUMENT_ELEC_PIANO'
+export const INSTRUMENT_FLUTE = 'INSTRUMENT_FLUTE'
+export const INSTRUMENT_HONKY_TONK_PIANO = 'INSTRUMENT_HONKY_TONK_PIANO'
+export const INSTRUMENT_METAL_GUITAR = 'INSTRUMENT_METAL_GUITAR'
+export const INSTRUMENT_POP_LEAD = 'INSTRUMENT_POP_LEAD'
+export const INSTRUMENT_SYNTH_ACCORDION = 'INSTRUMENT_SYNTH_ACCORDION'
+export const INSTRUMENT_SYNTH_BRASS = 'INSTRUMENT_SYNTH_BRASS'
+export const INSTRUMENT_SYNTH_LEAD = 'INSTRUMENT_SYNTH_LEAD'
 
 export const INSTRUMENT_OPTIONS = [
   {
@@ -71,8 +82,53 @@ export const INSTRUMENT_OPTIONS = [
   {
     label: "Piano 2",
     value: INSTRUMENT_PIANO2,
-  }
+  },
+  {
+    label: "Accordion",
+    value: INSTRUMENT_ACCORDION,
+  },
+  {
+    label: "Piano 2",
+    value: INSTRUMENT_BASSOON,
+  },
+  {
+    label: "Piano 2",
+    value: INSTRUMENT_CELLO,
+  },
+  {
+    label: "Electric Piano",
+    value: INSTRUMENT_ELEC_PIANO,
+  },
+  {
+    label: "Flute",
+    value: INSTRUMENT_FLUTE,
+  },
+  {
+    label: "Honky Tonk Piano",
+    value: INSTRUMENT_HONKY_TONK_PIANO,
+  },
+  {
+    label: "Metal Guitar",
+    value: INSTRUMENT_METAL_GUITAR,
+  },
+  {
+    label: "Pop Lead",
+    value: INSTRUMENT_POP_LEAD,
+  },
+  {
+    label: "Synth Accordion",
+    value: INSTRUMENT_SYNTH_ACCORDION,
+  },
+  {
+    label: "Synth Brass",
+    value: INSTRUMENT_SYNTH_BRASS,
+  },
+  {
+    label: "Synth Lead",
+    value: INSTRUMENT_SYNTH_LEAD,
+  },
 ]
+
 
 export const INSTRUMENTS = {
   [INSTRUMENT_PIANO1]: {
@@ -87,6 +143,122 @@ export const INSTRUMENTS = {
     attack: 0,
     release: 0.3,
   },
+  [INSTRUMENT_ACCORDION]: {
+    urls: {
+      C1: "accordionC1.wav",
+      C2: "accordionC2.wav",
+    },
+    baseUrl: "/instruments/ACCORDION/",
+    attack: 0,
+    release: 0.3,
+  },
+  [INSTRUMENT_BASSOON]: {
+    urls: {
+      C2: "bassoonC2.wav",
+      C3: "bassoonC3.wav",
+      C4: "bassoonC4.wav",
+      G2: "bassoonG2.wav",
+    },
+    baseUrl: "/instruments/BASSOON/",
+    attack: 0,
+    release: 0.3,
+  },
+  [INSTRUMENT_CELLO]: {
+    urls: {
+      C2: "cello2C2.wav",
+      C3: "cello2C3.wav",
+      C4: "cello2C4.wav",
+    },
+    baseUrl: "/instruments/CELLO/",
+    attack: 0,
+    release: 0.3,
+  },
+  [INSTRUMENT_ELEC_PIANO]: {
+    urls: {
+      G1: "epianoG1.wav",
+      C2: "epianoC2.wav",
+      C3: "epianoC3.wav",
+      G2: "epianoG2.wav",
+      G3: "epianoG3.wav",
+    },
+    baseUrl: "/instruments/ELEC-PIANO/",
+    attack: 0,
+    release: 0.3,
+  },
+  [INSTRUMENT_FLUTE]: {
+    urls: {
+      C1: "fluteC1.wav",
+      C2: "fluteC2.wav",
+    },
+    baseUrl: "/instruments/FLUTE/",
+    attack: 0,
+    release: 0.3,
+  },
+  [INSTRUMENT_HONKY_TONK_PIANO]: {
+    urls: {
+      G1: "honkypianoG1.wav",
+      C2: "honkypianoC2.wav",
+      C3: "honkypianoC3.wav",
+      G2: "honkypianoG2.wav",
+      G3: "honkypianoG3.wav",
+      C4: "honkypianoC4.wav",
+    },
+    baseUrl: "/instruments/HONKY-TONK-PIANO/",
+    attack: 0,
+    release: 0.3,
+  },
+  [INSTRUMENT_METAL_GUITAR]: {
+    urls: {
+      G1: "metalguitarG1.wav",
+      C2: "metalguitarC2.wav",
+      C3: "metalguitarC3.wav",
+      G2: "metalguitarG2.wav",
+    },
+    baseUrl: "/instruments/METAL-GUITAR/",
+    attack: 0,
+    release: 0.3,
+  },
+  [INSTRUMENT_POP_LEAD]: {
+    urls: {
+      G1: "popleadG1.wav",
+      G2: "popleadG2.wav",
+      G3: "popleadG3.wav",
+    },
+    baseUrl: "/instruments/POP-LEAD/",
+    attack: 0,
+    release: 0.3,
+  },
+  [INSTRUMENT_SYNTH_ACCORDION]: {
+    urls: {
+      G1: "synthaccordionG1.wav",
+      G2: "synthaccordionG2.wav",
+      G3: "synthaccordionG3.wav",
+    },
+    baseUrl: "/instruments/SYNTH-ACCORDION/",
+    attack: 0,
+    release: 0.3,
+  },
+  [INSTRUMENT_SYNTH_BRASS]: {
+    urls: {
+      C2: "synthbrassC2.wav",
+      C3: "synthbrassC3.wav",
+      C4: "synthbrassC4.wav",
+    },
+    baseUrl: "/instruments/SYNTH-BRASS/",
+    attack: 0,
+    release: 0.3,
+  },
+  [INSTRUMENT_SYNTH_LEAD]: {
+    urls: {
+      G1: "synthleadG1.wav",
+      C2: "synthleadC2.wav",
+      C3: "synthleadC3.wav",
+      G3: "synthleadG3.wav",
+    },
+    baseUrl: "/instruments/SYNTH-LEAD/",
+    attack: 0,
+    release: 0.3,
+  },
   [INSTRUMENT_PIANO2]: {
     urls: {
       A1: "A1.mp3",
@@ -96,7 +268,18 @@ export const INSTRUMENTS = {
     attack: 0.2,
     release: 0.1,
   }
-  
 }
 
-export type InstrumentType = typeof INSTRUMENT_PIANO1 | typeof INSTRUMENT_PIANO2;
+export type InstrumentType = typeof INSTRUMENT_PIANO1 |
+  typeof INSTRUMENT_PIANO2 |
+  typeof INSTRUMENT_ACCORDION |
+  typeof INSTRUMENT_BASSOON |
+  typeof INSTRUMENT_CELLO |
+  typeof INSTRUMENT_ELEC_PIANO |
+  typeof INSTRUMENT_FLUTE |
+  typeof INSTRUMENT_HONKY_TONK_PIANO |
+  typeof INSTRUMENT_METAL_GUITAR |
+  typeof INSTRUMENT_POP_LEAD |
+  typeof INSTRUMENT_SYNTH_ACCORDION |
+  typeof INSTRUMENT_SYNTH_BRASS |
+  typeof INSTRUMENT_SYNTH_LEAD;
