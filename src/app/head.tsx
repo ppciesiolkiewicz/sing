fc7179469e631e9ed390069f5815c6355e40d8b8
Piotr Ciesiolkiewicz <piotr.c@welysn.com>
2023-03-27 23:51:43 +1100

    force landscape mode

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
       <title>Sing5</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Learn singing through vocal exercises" />
+      <link rel="manifest" href="/manifest.json" />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
