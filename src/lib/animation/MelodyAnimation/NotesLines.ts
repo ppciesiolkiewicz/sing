1a763293dcae9958554bd6df5ecf7325e8739e59
Piotr Ciesiolkiewicz <piotr.c@welysn.com>
2023-03-30 21:04:25 +1100

    Improve NoteLines

--- src/lib/animation/MelodyAnimation/NotesLines.ts
+++ src/lib/animation/MelodyAnimation/NotesLines.ts
@@ -17,23 +17,24 @@ export default class NotesLines {
       text: string;
     },
   }) {
+    const fontSize = 12 * window.devicePixelRatio;
     notes.forEach((note) => {
       const noteYPosition = freqToCanvasYPosition(note.freq!);
       const line = new Path.Line(
-        new Point(0, noteYPosition),
+        new Point(fontSize * 3, noteYPosition),
         new Point(view.size.width, noteYPosition),
       );
       line.strokeWidth = 1 * window.devicePixelRatio; // TODO:
       line.strokeColor = new paper.Color(theme.line);
       line.strokeCap = 'round';
 
-      const text = new PointText(new Point(15 * window.devicePixelRatio, noteYPosition));
+      const text = new PointText(new Point(15 * window.devicePixelRatio, noteYPosition + fontSize/4));
       text.content = note.name;
       text.style = {
           ...text.style,
           fontFamily: 'Courier New',
           fontWeight: 'bold',
-          fontSize: 12 * window.devicePixelRatio,
+          fontSize,
           fillColor: new paper.Color(theme.text),
           justification: 'center'
       };
